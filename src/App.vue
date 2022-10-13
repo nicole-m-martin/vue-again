c1fed4113f9f6b6f5a1f9605be596c6bfa8bf5e4
Nicole Martin <71411431+nicole-m-martin@users.noreply.github.com>
2022-10-13 16:50:48 -0700

    Add Counter and Router

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ul>
+      <li><RouterLink :to="{ name: 'home' }">Home</RouterLink></li>
+      <li><RouterLink :to="{ name: 'about' }">About</RouterLink></li>
+    </ul>
+  </div>
+  <router-view />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>About</h1>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error beatae
+    soluta exercitationem animi eius quaerat mollitia facilis assumenda tempora
+    provident atque molestiae reiciendis voluptas qui quidem distinctio dolore,
+    unde dolor.
+  </p>
+</template>
